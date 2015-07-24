82458148caa37123b65a4ceb5a12389ba1d5b154
Boris Fritscher <boris@fritscher.ch>
2015-07-24 16:08:06 +0200

    some apis

--- app/server.ts
+++ app/server.ts
@@ -7,6 +7,7 @@ import express = require('express');
 import bodyParser = require('body-parser');
 import errorHandler = require('errorhandler');
 import sqlite3 = require('sqlite3');
+import path = require('path');
 
 var app = express();
 app.use(cors({
@@ -28,6 +29,8 @@ if (env === 'development') {
 else if (env === 'production') {
     app.use(express.static(__dirname + '/public'));
 }
+
+var project = 'test'; //sqla-2015-exa';
 
 app.get('/', (req, res) => {
     res.send('Hello World3!');
@@ -79,25 +82,53 @@ export /download pdfs
 
 */
 
+/*
+ZONE_FRAME=>1,
+ZONE_NAME=>2,
+ZONE_DIGIT=>3, //top of the page id
+ZONE_BOX=>4,
+*/
 
-app.get('/students', (req, res) => {
-    // LIST OF STUDENTS with their name field and if matched
-    var db = new sqlite3.Database('app/projects/test/data/capture.sqlite', (err) => {
+function database(res, callback){
+    var db = new sqlite3.Database('app/projects/' + project + '/data/capture.sqlite', (err) => {
         if (err){
             res.status(500).send(err);
         }
         db.serialize(() => {
-            db.exec("ATTACH DATABASE 'app/projects/test/data/layout.sqlite' AS layout");
-            db.exec("ATTACH DATABASE 'app/projects/test/data/association.sqlite' AS assoc");
+            db.exec("ATTACH DATABASE 'app/projects/" + project + "/data/layout.sqlite' AS layout");
+            db.exec("ATTACH DATABASE 'app/projects/" + project + "/data/association.sqlite' AS assoc");
+            var dbHandled = (method, query, params, success) => {
+                var internalCallback = (err, rows) => {
+                    if (err){
+                        res.status(500).send(err);
+                    }
+                    if (success){
+                        success(rows);
+                    } else {
+                        params(rows);
+                    }
+                };
+                if (success){
+                    db[method](query, params, internalCallback);
+                } else {
+                    db[method](query, internalCallback);
+                }
+            };
+            callback(dbHandled);
+        });
+    });
+}
 
-            var query = 'SELECT p.student, p.page, p.copy, z.image, a.manual, a.auto '
+app.get('/students', (req, res) => {
+    // LIST OF STUDENTS with their name field and if matched
+    database(res, (db) => {
+        var query = 'SELECT p.student, p.page, p.copy, z.image, a.manual, a.auto '
             + 'FROM capture_page p JOIN layout.layout_namefield l ON p.student=l.student AND p.page = l.page '
             + 'LEFT JOIN capture_zone z ON z.type = 2 AND z.student = p.student AND z.page = p.page AND z.copy = p.copy '
             + 'LEFT JOIN assoc.association_association a ON a.student = p.student AND a.copy = p.copy';
 
-            db.all(query, (err, rows) => {
-                res.json(rows);
-            });
+        db('all', query, (rows) => {
+            res.json(rows);
         });
     });
 });
@@ -105,51 +136,118 @@ app.get('/students', (req, res) => {
 /* CAPTURE*/
 
 /*
-def sql = Sql.newInstance("jdbc:sqlite:webapps/amcui/project/data/capture.sqlite", "org.sqlite.JDBC")
-sql.execute("ATTACH DATABASE 'webapps/amcui/project/data/layout.sqlite' AS layout")
-sql.execute("ATTACH DATABASE 'webapps/amcui/project/data/association.sqlite' AS assoc")
+debug "Removing data capture for ".pageids_string(@id);
+	#
+	# 1) get image files generated, and remove them
+	#
+	my $crdir=$shortcuts->absolu($projet{'options'}->{'cr'});
+	my @files=();
+	#
+	# scan file
+	push @files,$shortcuts->absolu($projet{'_capture'}->get_scan_page(@id));
+	#
+	# layout image, in cr directory
+	push @files,$crdir.'/'
+	  .$projet{'_capture'}->get_layout_image(@id);
+	#
+	# annotated scan
+	push @files,$crdir.'/corrections/jpg/'
+	  .$projet{'_capture'}->get_annotated_page(@id);
+	#
+	# zooms
+	push @files,map { $crdir.'/zooms/'.$_ } grep { defined($_) }
+	  ($projet{'_capture'}->get_zones_images(@id,ZONE_BOX));
+	#
+	for (@files) {
+	  if (-f $_) {
+	    debug "Removing $_";
+	    unlink($_);
+	  }
+	}
+	#
+	# 2) remove data from database
+	#
+	$projet{'_capture'}->delete_page_data(@id);
 
-def query = '''SELECT p.student, p.page, p.copy, p.mse, p.timestamp_auto, p.timestamp_manual, 8 s FROM
-capture_page p
-*/
-/*LEFT JOIN capture_zone z ON z.type = 2 AND z.student = p.student AND z.page = p.page AND z.copy = p.copy
-GROUP BY p.student, p.page, p.copy, p.mse, p.timestamp_auto, p.timestamp_manual */
-/*
-'''
-if(params.student && params.page && params.copy) {
-  query =  "SELECT p.* FROM capture_page p WHERE p.student=?1 AND p.page=?2 AND p.copy=?3"
-  out << new JsonBuilder(sql.rows(query, [params.student, params.page, params.copy]))
+	if($projet{'options'}->{'auto_capture_mode'} == 1) {
+	  $projet{'_scoring'}->delete_scoring_data(@id[0,2]);
+	  $projet{'_association'}->delete_association_data(@id[0,2]);
+	}
 
-}else{
-   out << new JsonBuilder(sql.rows(query))
-}
-*/
-/*
-
-{'sql'=>"SELECT student,page,copy,mse,timestamp_auto,timestamp_manual"
-      .",CASE WHEN timestamp_auto>0 AND mse>? THEN ?"
-      ."      ELSE ?"
-      ."  END AS mse_color"
-      .",CASE WHEN timestamp_manual>0 THEN ?"
-      ."      WHEN timestamp_auto>0 THEN ?"
-      ."      ELSE ?"
-      ."  END AS color"
-      .",CASE WHEN timestamp_manual>0 THEN timestamp_manual"
-      ."      ELSE timestamp_auto"
-      ."  END AS timestamp"
-      .",(SELECT MIN(ABS(1.0*black/total-?))"
-      ."   FROM $t_zone"
-      ."   WHERE $t_zone.student=$t_page.student"
-      ."     AND $t_zone.page=$t_page.page AND $t_zone.copy=$t_page.copy"
-      ."     AND $t_zone.type=? AND total>0) AS delta"
-      .",(SELECT MIN(ABS(1.0*black/total-?))"
-      ."   FROM $t_zone"
-      ."   WHERE $t_zone.student=$t_page.student"
-      ."     AND $t_zone.page=$t_page.page AND $t_zone.copy=$t_page.copy"
-      ."     AND $t_zone.type=? AND total>0) AS delta_up"
-      ." FROM $t_page"},
 
 */
+
+app.get('/missing', (req, res) => {
+    database(res, (db) => {
+        //TODO in future check that role=1 version>1.2.1
+        var query = 'SELECT a.student as student, a.page as page, a.copy as copy, ok.page IS NULL as missing '
+        + 'FROM (SELECT enter.student, enter.page, p.copy FROM ( '
+        + '    SELECT student, page '
+        + '    FROM layout_namefield '
+        + '    UNION '
+        + '    SELECT student, page '
+        + '    FROM layout_box) enter, '
+        + '    (SELECT student, copy FROM capture_page GROUP BY student, copy) p'
+        + '  WHERE p.student = enter.student) a '
+        + 'LEFT JOIN capture_page ok ON a.student = ok.student AND a.page = ok.page AND ok.copy = a.copy '
+        + 'ORDER BY student, copy, page';
+
+        db('all', query, (rows) => {
+            var seenTotal = [];
+            var seenMissing = [];
+            var results = rows.reduce((result, page) => {
+                var id = page.student + page.copy;
+                if (seenTotal.indexOf(id) < 0){
+                  result.complete += 1;
+                  seenTotal.push(id);
+                }
+                if (page.missing === 1){
+                  result.missing.push(page);
+                  if (seenMissing.indexOf(id) < 0){
+                    result.complete -= 1;
+                    result.incomplete += 1;
+                    seenMissing.push(id);
+                  }
+                }
+                return result;
+            }, {complete: 0, incomplete: 0, missing: []});
+
+            var query2 = 'SELECT * FROM capture_failed';
+            db('all', query2, (rows) => {
+                results.failed = rows;
+                res.json(results);
+            });
+        });
+    });
+});
+
+
+app.get('/capture', (req, res) => {
+    database(res, (db) => {
+        var threshold = 0.15;
+        + '(SELECT ROUND(10* COALESCE(($threshold - MIN(ABS(1.0*black/total - $threshold)))/ $threshold, 0), 1) '
+        + 'FROM capture_zone WHERE student=p.student AND page=p.page AND copy=p.copy AND type=4) s '
+        + 'FROM capture_page p ORDER BY p.student, p.page, p.copy';
+
+        db('all', query, {$threshold: threshold}, (rows) => {
+            res.json(rows);
+        });
+    });
+});
+
+app.get('/capture/:student/:page\::copy', (req, res) => {
+    database(res, (db) => {
+        var query = 'SELECT * FROM capture_page WHERE student=$student AND page=$page AND copy=$copy';
+        db('get', query, {$student: req.params.student, $page: req.params.page, $copy: req.params.copy}, (row) => {
+            res.json(row);
+        });
+    });
+});
+
+app.get('/static/:image', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../app/projects/' + project + '/cr/' + req.params.image));
+});
+
 
 /* ZONES */
 
@@ -158,34 +256,41 @@ def sql = Sql.newInstance("jdbc:sqlite:webapps/amcui/project/data/capture.sqlite
 //TODO handle errors
 
 if(params.student && params.page && params.copy) {
-    out << new JsonBuilder(sql.rows('''SELECT z.id_a AS question, z.id_b AS answer, z.total, z.black,
-        z.manual, max(CASE WHEN p.corner = 1 THEN p.x END) as x0,
-        max(CASE WHEN p.corner = 1 THEN p.y END) as y0,
-        max(CASE WHEN p.corner = 2 THEN p.x END) as x1,
-        max(CASE WHEN p.corner = 2 THEN p.y END) as y1,
-        max(CASE WHEN p.corner = 3 THEN p.x END) as x2,
-        max(CASE WHEN p.corner = 3 THEN p.y END) as y2,
-        max(CASE WHEN p.corner = 4 THEN p.x END) as x3,
-        max(CASE WHEN p.corner = 4 THEN p.y END) as y3
-        FROM capture_zone AS z
-        JOIN capture_position as p ON z.zoneid=p.zoneid
-        WHERE z.student=?1 AND z.page=?2 AND z.copy=?3 AND z.type=4 AND p.type=1
-        GROUP BY z.zoneid, z.id_a, z.id_b, z.total, z.black, z.manual
-        ORDER BY min(p.y), min(p.y);''',
+    out << new JsonBuilder(sql.rows(''';''',
         [params.student, params.page, params.copy]))
 }else{
   println 'error required parameters: student, page and copy'
 }
 */
-
+app.get('/zones/:student/:page\::copy', (req, res) => {
+    database(res, (db) => {
+        var query = 'SELECT z.id_a AS question, z.id_b AS answer, z.total, z.black, '
+        + 'z.manual, max(CASE WHEN p.corner = 1 THEN p.x END) as x0, '
+        + 'max(CASE WHEN p.corner = 1 THEN p.y END) as y0, '
+        + 'max(CASE WHEN p.corner = 2 THEN p.x END) as x1, '
+        + 'max(CASE WHEN p.corner = 2 THEN p.y END) as y1, '
+        + 'max(CASE WHEN p.corner = 3 THEN p.x END) as x2, '
+        + 'max(CASE WHEN p.corner = 3 THEN p.y END) as y2, '
+        + 'max(CASE WHEN p.corner = 4 THEN p.x END) as x3, '
+        + 'max(CASE WHEN p.corner = 4 THEN p.y END) as y3 '
+        + 'FROM capture_zone AS z '
+        + 'JOIN capture_position as p ON z.zoneid=p.zoneid '
+        + 'WHERE z.student=$student AND z.page=$page AND z.copy=$copy AND z.type=4 AND p.type=1 '
+        + 'GROUP BY z.zoneid, z.id_a, z.id_b, z.total, z.black, z.manual '
+        + 'ORDER BY min(p.y), min(p.y)';
+        db('all', query, {$student: req.params.student, $page: req.params.page, $copy: req.params.copy}, (rows) => {
+            res.json(rows);
+        });
+    });
+});
 
 /* REPORT */
-
+/*
 def sql = Sql.newInstance("jdbc:sqlite:webapps/amcui/project/data/capture.sqlite", "org.sqlite.JDBC")
 //sql.execute("ATTACH DATABASE 'webapps/amcui/project/data/layout.sqlite' AS layout")
 sql.execute("ATTACH DATABASE 'webapps/amcui/project/data/scoring.sqlite' AS scoring")
 //sql.execute("ATTACH DATABASE 'webapps/amcui/project/data/association.sqlite' AS assoc")
-
+*/
 
 /*
 original query from AMC
