2d4663c33212b02e85ab8fdb71e964f0750c5719
Boris Fritscher <boris@fritscher.ch>
2015-08-10 18:50:45 +0200

    fix path in bat

--- app/assets/print.bat
+++ app/assets/print.bat
@@ -1,1 +1,1 @@
-for %%p in ("sujets/*.pdf") do "c:\Program Files (x86)\Foxit Software\Foxit Reader\FoxitReader.exe" /t "%cd%\%%p" \\srv-print1\PR-Canon-PCL
+for %%p in ("sujets/*.pdf") do "c:\Program Files (x86)\Foxit Software\Foxit Reader\FoxitReader.exe" /t "%cd%\sujets\%%p" \\srv-print1\PR-Canon-PCL
