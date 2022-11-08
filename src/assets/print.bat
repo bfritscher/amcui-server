5974e6a8ad6ce68f44612889e68d1d4b09e71a1a
Boris Fritscher <boris@fritscher.ch>
2022-11-08 07:50:58 +0100

    update foxitreader location and default printer

--- src/assets/print.bat
+++ src/assets/print.bat
@@ -1,1 +1,1 @@
-for %%p in ("sujets/*.pdf") do "c:\Program Files (x86)\Foxit Software\Foxit Reader\FoxitReader.exe" /t "%cd%\sujets\%%p" \\srv-print1\PR-Canon-PCL
+for %%p in ("sujets/*.pdf") do "c:\Program Files (x86)\Foxit Software\Foxit PDF Reader\FoxitPDFReader.exe" /t "%cd%\sujets\%%p" \\srv-printcan\PR-Canon-PCL
