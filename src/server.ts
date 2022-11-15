0ec9eebd6a19c21bfc5b78ad839e95a6d3116f10
Boris Fritscher <boris@fritscher.ch>
2022-11-15 09:40:55 +0100

    fix wrong type nb

--- src/server.ts
+++ src/server.ts
@@ -2925,7 +2925,7 @@ REPORT_PRINTED_COPY         => 3,
 REPORT_ANONYMIZED_PDF       => 4,
                     */
                     db.all(
-                      'SELECT file FROM report_student WHERE student=$student AND copy=$copy AND type=3',
+                      'SELECT file FROM report_student WHERE student=$student AND copy=$copy AND type=1',
                       {$student: student, $copy: copy},
                       (_err, rows) => {
                         let filename = undefined;
